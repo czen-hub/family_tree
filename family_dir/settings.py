fb3cd5e8f331e62f98e4c1296c83c27c750b4ba9
tenzin <czen919@gmail.com>
2026-04-09 01:01:22 -0700

    fix requirements and email settings

--- family_dir/settings.py
+++ family_dir/settings.py
@@ -133,8 +133,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('czen919@gmail.com', '')
-EMAIL_HOST_PASSWORD = os.environ.get('kvkc ayvj oiho rwpl', '')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = 'czen919@gmail.com'
 
 # Default primary key field type
