20edb5de38016669a72d6bd33cc97a74d970ed90
tenzin <czen919@gmail.com>
2026-04-09 01:38:57 -0700

    settings email changed

--- family_dir/settings.py
+++ family_dir/settings.py
@@ -131,8 +131,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 DEFAULT_FROM_EMAIL = 'czen919@gmail.com'
